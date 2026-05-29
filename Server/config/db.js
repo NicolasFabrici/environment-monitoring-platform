883a75e899a103ca99635598857a2d5f4d63e1f5
Nicolas Fabrici <nicolasfabrici@gmail.com>
2026-05-29 10:12:30 -0300

    Configure production database

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,12 +1,11 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10
+    port: process.env.DB_PORT
 });
 
-module.exports = connection.promise();
+module.exports = pool.promise();
